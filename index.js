ed8c8ab1f0b22148b77e4bd893f6fb6047955d99
Valdeci97 <moura97.valdeci@gmail.com>
2022-03-17 11:12:20 -0300

    New: create a dokcer container and starts deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get(
+  '/',
+  (_req, res) => res
+    .send(`<h1>Sou um container docker rodando na porta ${PORT}</h1>`),
+);
+
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`I'm online here ${PORT}`));
